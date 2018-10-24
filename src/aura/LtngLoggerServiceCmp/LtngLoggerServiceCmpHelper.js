b6e78805320103d965c9bee41c3faa2a272bfe1f
Shreyas <shre100789@gmail.com>
2018-10-23 23:27:15 -0400

    Added some more metadata and fixed issue with controller action getting called

--- src/aura/LtngLoggerServiceCmp/LtngLoggerServiceCmpHelper.js
+++ src/aura/LtngLoggerServiceCmp/LtngLoggerServiceCmpHelper.js
@@ -6,18 +6,21 @@
      * @param {[type]} cmpName component generating the log
      */
     addLog : function(component, event, logMsg, severity, cmpName) {
-        var action = component.get("c.addLog");
+        console.log("LtngLoggerServiceCmpHelper->addLog: " + logMsg + ',' + severity + ',' + cmpName);
+        var action = component.get("c.createLog");
         action.setParams({"logMsg" : logMsg,
             "severity" : severity,
             "cmpName" : cmpName});
         action.setCallback(this, function(response) {
             var state = response.getState();
+            console.log("state: " + JSON.stringify(response.getState()));
             if(state === "SUCCESS") {
-
+                console.log("Log successfully created.");
             } else if(state === "ERROR") {
                 console.log("error adding log: " + response.getError()[0].message);
             }
         });
         $A.enqueueAction(action);
+        console.log('enqueued');
     }
 })
